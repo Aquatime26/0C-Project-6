a278014c02517c44b86b820f2637e9208d18e7f3
Alexis.Avril <alexisavril@sfr.fr>
2025-10-08 08:17:25 +0200

    Modifications on modifyThings function

--- backend/controllers/stuff.js
+++ backend/controllers/stuff.js
@@ -1,5 +1,13 @@
 const Thing = require('../models/Thing');
 
+//Récupérer tous les éléments
+exports.getAllThings = (req, res, next) => {
+  Thing.find()
+    .then(things => res.status(200).json(things))
+    .catch(error => res.status(400).json({ error }));
+}
+
+//Créer un nouvel élément
 exports.createThing = (req, res, next) => {
   const thingObject = JSON.parse(req.body.thing);
   delete thingObject._id;
@@ -15,26 +23,39 @@ exports.createThing = (req, res, next) => {
   .catch(error => { res.status(400).json( { error })})
 }
 
-exports.modifieThing = (req, res, next) => {
-  Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
-    .catch(error => res.status(400).json({ error }));
-}
-
-exports.deleteThing = (req, res, next) => {
-  Thing.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
-    .catch(error => res.status(400).json({ error }));
-}
-
+//Récupérer un élément
 exports.getOneThing = (req, res, next) => {
   Thing.findOne({ _id: req.params.id })
     .then(thing => res.status(200).json(thing))
     .catch(error => res.status(404).json({ error }));
 }
 
-exports.getAllThings = (req, res, next) => {
-  Thing.find()
-    .then(things => res.status(200).json(things))
+//Modifier un élément
+exports.modifieThing = (req, res, next) => {
+  const thingObject = req.file ? {
+    ...JSON.parse(req.body.thing),
+    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+  } : { ...req.body };
+
+  delete thingObject._userId;
+  Thing.findOne({ _id: req.params.id })
+    .then((thing) => {
+      if (thing.userId != req.auth.userId) {
+        res.status(401).json({ message : 'Not authorized'});
+      } else {
+        Thing.updateOne({ _id: req.params.id }, { ...thingObject, _id: req.params.id })
+          .then(() => res.status(200).json({ message : 'Objet modifié!'}))
+          .catch(error => res.status(401).json({ error }));
+      }
+    })
+    .catch((error) => {
+      res.status(400).json({ error }); 
+  });
+}
+
+//Supprimer un élément
+exports.deleteThing = (req, res, next) => {
+  Thing.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
     .catch(error => res.status(400).json({ error }));
 }
--- backend/routes/stuff.js
+++ backend/routes/stuff.js
@@ -6,20 +6,20 @@ const multer = require('../middleware/multer-config');
 const stuffCtrl = require('../controllers/control');
 
 // Routes
-//Créer un nouvel élément
-router.post('/', auth, multer, stuffCtrl.createThing);
-
-//Modifier un élément
-router.put('/:id', auth, stuffCtrl.modifieThing);
-
-//Supprimer un élément
-router.delete('/:id', auth, stuffCtrl.deleteThing);
 
 //Récupérer tous les éléments
 router.get('/' , auth, stuffCtrl.getAllThings);
 
+//Créer un nouvel élément
+router.post('/', auth, multer, stuffCtrl.createThing);
+
 //Récupérer un élément
 router.get('/:id', auth, stuffCtrl.getOneThing);
 
+//Modifier un élément
+router.put('/:id', auth, multer, stuffCtrl.modifieThing);
+
+//Supprimer un élément
+router.delete('/:id', auth, stuffCtrl.deleteThing);
 
 module.exports = router;
